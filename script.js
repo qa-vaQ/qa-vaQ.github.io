467096d46c0bc42d367de28574962af300d59006
qa-vaQ <45640607+qa-vaQ@users.noreply.github.com>
2019-03-03 18:11:40 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var counter = 0;
-var colors = ["#ff1e1e", "#FF7F00", "#FFFF00", "#00FF00", "#00FFFF", "#2323ff", "#9311ff"];
+var colors = ["#2323ff", "#FF7F00", "#FFFF00", "#00FF00", "#00FFFF", "#ff1e1e", "#9311ff"];
 
 function setup() {
   setInterval(changeColor, 3000);
