df41b67b29e05d3c46dafb648a633e6e588884de
qa-vaQ <45640607+qa-vaQ@users.noreply.github.com>
2019-10-14 10:57:16 +0100

    Create script.js

---
+++ Colour/script.js
@@ -0,0 +1,17 @@
+var counter = 0;
+var colors = ["#00FFFF", "#2323ff", "#9311ff", "#ff1e1e", "#FF7F00", "#FFFF00", "#00FF00"];
+
+function setup() {
+  setInterval(changeColor, 3000);
+ }
+ 
+ function changeColor() {
+  var body = document.body;
+  body.style.backgroundColor = colors[counter];
+  
+  if(counter == 6) {
+    counter = 0;
+  } else {
+    counter++;
+  }
+}
