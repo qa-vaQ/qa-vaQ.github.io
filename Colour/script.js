e87aa7b3a3d4c8af7052ce280962120fff815d17
qa-vaQ <45640607+qa-vaQ@users.noreply.github.com>
2020-02-03 11:35:07 +0000

    Update script.js

--- Colour/script.js
+++ Colour/script.js
@@ -1,17 +1,18 @@
 var counter = 0;
-var colors = ["#0000FF,#2E2B5F,#8B00FF,#FF0000,#FF7F00,#FFFF00,#00FF00"];
+var colors = ["#00FFFF", "#2323ff", "#9311ff", "#ff1e1e", "#FF7F00", "#FFFF00", "#00FF00"];
 
 function setup() {
-  setInterval(changeColor, 500);
+  setInterval(changeColor, 3000);
  }
  
  function changeColor() {
   var body = document.body;
   body.style.backgroundColor = colors[counter];
   
-  if(counter == 7) {
+  if(counter == 6) {
     counter = 0;
   } else {
     counter++;
   }
 }
+  
