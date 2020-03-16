119338d5f4eddd2be1c426be4086e2ea7052a12b
qa-vaQ <45640607+qa-vaQ@users.noreply.github.com>
2020-03-16 10:24:37 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 var counter = 0;
-var colors = ["#00FFFF", "#0080FF", "#0000FF", "#7F00FF", "#FF00FF", "#FF007F", "#FF0000", "#FF8000", "#FFFF00", "#80FF00", "#00FF00", "#00FF80"];
+var colors = ["#00FFFF", "#2323ff", "#9311ff", "#ff1e1e", "#FF7F00", "#FFFF00", "#00FF00"];
 
 function setup() {
-  setInterval(changeColor, 2500);
+  setInterval(changeColor, 3000);
  }
  
  function changeColor() {
   var body = document.body;
   body.style.backgroundColor = colors[counter];
   
-  if(counter == 12) {
+  if(counter == 6) {
     counter = 0;
   } else {
     counter++;
