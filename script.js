9f38ef2368d4d49d4171b555f1f722f25e36ff5e
qa-vaQ <45640607+qa-vaQ@users.noreply.github.com>
2021-09-30 20:02:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,225 @@
-var counter = 0;
-var colors = ["#00FFFF", "#2323ff", "#9311ff", "#ff1e1e", "#FF7F00", "#FFFF00", "#00FF00"];
-
-function setup() {
-  setInterval(changeColor, 3000);
- }
-
-var colour1 = localStorage.getItem(colour0);
-document.body.style.backgroundColor = colour1
-
-function changeColor() {
-  var body = document.body;
-  body.style.backgroundColor = colors[counter];
-  localStorage.setItem(body.style.backgroundColor, colour0);
-  if(counter == 6) {
-    counter = 0;
-  } else {
-    counter++;
-  }
-}
-function easteregg() {
-        var decider1 = document.getElementById('easteregg');
-        if(decider1.checked){
-            document.getElementById("body").style.transform = "rotate(180deg)";
+function load() {
+        document.getElementById("button0").disabled = true;
+      }
+      function home() {
+        document.getElementById("page1").style.display = "none";
+        document.getElementById("page2").style.display = "none";
+        document.getElementById("page3").style.display = "none";
+        document.getElementById("page4").style.display = "none";
+        document.getElementById("page5").style.display = "none";
+        document.getElementById("page6").style.display = "none";
+        document.getElementById("page7").style.display = "none";
+        document.getElementById("page8").style.display = "none";
+        document.getElementById("button0").disabled = true;
+        document.getElementById("button1").disabled = false;
+        document.getElementById("button2").disabled = false;
+        document.getElementById("button3").disabled = false;
+        document.getElementById("button4").disabled = false;
+      }
+      function profile() {
+        document.getElementById("page1").style.display = "block";
+        document.getElementById("page2").style.display = "none";
+        document.getElementById("page3").style.display = "none";
+        document.getElementById("page4").style.display = "none";
+        document.getElementById("page5").style.display = "none";
+        document.getElementById("page6").style.display = "none";
+        document.getElementById("page7").style.display = "none";
+        document.getElementById("page8").style.display = "none";
+        document.getElementById("button0").disabled = false;
+        document.getElementById("button1").disabled = true;
+        document.getElementById("button2").disabled = false;
+        document.getElementById("button3").disabled = false;
+        document.getElementById("button4").disabled = false;
+      }
+      function project() {
+        document.getElementById("page1").style.display = "none";
+        document.getElementById("page2").style.display = "block";
+        document.getElementById("page3").style.display = "none";
+        document.getElementById("page4").style.display = "none";
+        document.getElementById("page5").style.display = "none";
+        document.getElementById("page6").style.display = "none";
+        document.getElementById("page7").style.display = "none";
+        document.getElementById("page8").style.display = "none";
+        document.getElementById("button0").disabled = false;
+        document.getElementById("button1").disabled = false;
+        document.getElementById("button2").disabled = true;
+        document.getElementById("button3").disabled = false;
+        document.getElementById("button4").disabled = false;
+      }
+      function about() {
+        document.getElementById("page1").style.display = "none";
+        document.getElementById("page2").style.display = "none";
+        document.getElementById("page3").style.display = "block";
+        document.getElementById("page4").style.display = "none";
+        document.getElementById("page5").style.display = "none";
+        document.getElementById("page6").style.display = "none";
+        document.getElementById("page7").style.display = "none";
+        document.getElementById("page8").style.display = "none";
+        document.getElementById("button0").disabled = false;
+        document.getElementById("button1").disabled = false;
+        document.getElementById("button2").disabled = false;
+        document.getElementById("button3").disabled = true;
+        document.getElementById("button4").disabled = false;
+      }
+      function contact() {
+        document.getElementById("page1").style.display = "none";
+        document.getElementById("page2").style.display = "none";
+        document.getElementById("page3").style.display = "none";
+        document.getElementById("page4").style.display = "block";
+        document.getElementById("page5").style.display = "none";
+        document.getElementById("page6").style.display = "none";
+        document.getElementById("page7").style.display = "none";
+        document.getElementById("page8").style.display = "none";
+        document.getElementById("button0").disabled = false;
+        document.getElementById("button1").disabled = false;
+        document.getElementById("button2").disabled = false;
+        document.getElementById("button3").disabled = false;
+        document.getElementById("button4").disabled = true;
+      }
+      function project1() {
+        document.getElementById("page1").style.display = "none";
+        document.getElementById("page2").style.display = "none";
+        document.getElementById("page3").style.display = "none";
+        document.getElementById("page4").style.display = "none";
+        document.getElementById("page5").style.display = "block";
+        document.getElementById("page6").style.display = "none";
+        document.getElementById("page7").style.display = "none";
+        document.getElementById("page8").style.display = "none";
+        document.getElementById("button0").disabled = false;
+        document.getElementById("button1").disabled = false;
+        document.getElementById("button2").disabled = false;
+        document.getElementById("button3").disabled = false;
+        document.getElementById("button4").disabled = false;
+        document.getElementById("game1").disabled = true;
+      }
+      function project2() {
+        document.getElementById("page1").style.display = "none";
+        document.getElementById("page2").style.display = "none";
+        document.getElementById("page3").style.display = "none";
+        document.getElementById("page4").style.display = "none";
+        document.getElementById("page5").style.display = "none";
+        document.getElementById("page6").style.display = "block";
+        document.getElementById("page7").style.display = "none";
+        document.getElementById("page8").style.display = "none";
+        document.getElementById("button0").disabled = false;
+        document.getElementById("button1").disabled = false;
+        document.getElementById("button2").disabled = false;
+        document.getElementById("button3").disabled = false;
+        document.getElementById("button4").disabled = false;
+      }
+      function project3() {
+        document.getElementById("page1").style.display = "none";
+        document.getElementById("page2").style.display = "none";
+        document.getElementById("page3").style.display = "none";
+        document.getElementById("page4").style.display = "none";
+        document.getElementById("page5").style.display = "none";
+        document.getElementById("page6").style.display = "none";
+        document.getElementById("page7").style.display = "block";
+        document.getElementById("page8").style.display = "none";
+        document.getElementById("button0").disabled = false;
+        document.getElementById("button1").disabled = false;
+        document.getElementById("button2").disabled = false;
+        document.getElementById("button3").disabled = false;
+        document.getElementById("button4").disabled = false;
+      }
+      function project4() {
+        document.getElementById("page1").style.display = "none";
+        document.getElementById("page2").style.display = "none";
+        document.getElementById("page3").style.display = "none";
+        document.getElementById("page4").style.display = "none";
+        document.getElementById("page5").style.display = "none";
+        document.getElementById("page6").style.display = "none";
+        document.getElementById("page7").style.display = "none";
+        document.getElementById("page8").style.display = "block";
+        document.getElementById("button0").disabled = false;
+        document.getElementById("button1").disabled = false;
+        document.getElementById("button2").disabled = false;
+        document.getElementById("button3").disabled = false;
+        document.getElementById("button4").disabled = false;
+      }
+      //function projectlist(x, y) {
+      //  var projecttag1 = "project";
+      //  var projecttag2 = String(x);
+      //  var projecttag = projecttag1.concat(projecttag2);
+      //  document.getElementById(projecttag).style.display = "block";
+      //  var buttontag1 = "button";
+      //  var buttontag2 = String(x);
+      //  var buttontag = buttontag1.concat(buttontag2);
+      //  document.getElementById(buttontag).disabled = true;
+      //  for (x > y > 0, 10 > y > x) {
+      //    var pagetag1 = "page";
+      //    var pagetag2 = String(y);
+      //    var pagetag = pagetag1.concat(pagetag2);
+      //    document.getElementById(pagetag).style.display = "none";
+      //    var notbuttontag1 = "button";
+      //    var notbuttontag2 = String(y);
+      //    var notbuttontag = notbuttontag1.concat(notbuttontag2);
+      //    document.getElementById(notbuttontag).disabled = false;
+      //  };
+      //  console.log(projecttag);
+      //  console.log(buttontag);
+      //  console.log(pagetag);
+      //  console.log(notbuttontag);
+      //};
+      function easteregg() {
+              var decider1 = document.getElementById('easteregg');
+              if(decider1.checked){
+                document.getElementById("body").style.transform = "rotate(180deg)";
+              } else {
+                document.getElementById("body").style.transform = "rotate(0deg)";
+              }
+          }
+      function easteregg2() {
+              var decider2 = document.getElementById('easteregg2');
+              if(decider2.checked){
+                  document.getElementById("body").style.filter = "invert(1)";
+              } else {
+                document.getElementById("body").style.filter = "invert(0)";
+              }
+          }
+      function easteregg3() {
+              var decider3 = document.getElementById('easteregg3');
+              if(decider3.checked){
+                document.getElementById("body").className = "foregroundspin";
+              } else {
+                document.getElementById("body").className = "foreground";
+              }
+      }
+      function easteregg31() {
+              var decider31 = document.getElementById('easteregg31');
+              if(decider31.checked){
+                document.getElementById("body").className = "foregroundspin1";
+              } else {
+                document.getElementById("body").className = "foreground";
+              }
+      }
+      function settingsopen() {
+              document.getElementById("settings").style.display = "block";
+      }
+      function settingsclose() {
+              document.getElementById("settings").style.display = "none";
+      }
+      var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down',
+        65: 'a',
+        66: 'b'
+      };
+      var konamiCode = ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'b', 'a'];
+      var konamiCodePosition = 0;
+      document.addEventListener('keydown', function(e) {
+        var key = allowedKeys[e.keyCode];
+        var requiredKey = konamiCode[konamiCodePosition];
+        if (key == requiredKey) {
+          konamiCodePosition++;
+          if (konamiCodePosition == konamiCode.length) {
+            settingsopen();
+            alert("Cheat Code Recognised");
+            konamiCodePosition = 0;
+          }
         } else {
-          document.getElementById("body").style.transform = "rotate(0deg)";
+          konamiCodePosition = 0;
         }
-    }
-function easteregg2() {
-        var decider2 = document.getElementById('easteregg2');
-        if(decider2.checked){
-            document.getElementById("body").style.filter = "invert(1)";
-        } else {
-          document.getElementById("body").style.filter = "invert(0)";
-        }
-    }
-function settingsopen() {
-        document.getElementById("settings").style.display = "block";
-    }
-function settingsclose() {
-        document.getElementById("settings").style.display = "none";
-    }
-  
+      });
